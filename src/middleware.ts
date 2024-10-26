2477c89b650739b24c05d5b48a70a99c31cbbf30
Carlos6464 <adrianosodre23@gmail.com>
2024-10-26 03:14:24 -0300

    ajsute

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getCookieServer } from "@/lib/cookieServer";
 import { api } from "@/services/api";
 
-export async function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl;
-    const token = req.cookies.get("tokenName")?.value; // Certifique-se de substituir "tokenName" pelo nome correto do cookie
+export async function middleware(req: NextRequest){
 
-    if (pathname.startsWith("/_next")) return NextResponse.next();
+    const { pathname} = req.nextUrl;
+    const token = getCookieServer();
 
-    // Impedir usuários logados de acessar a página de login ("/") e cadastro ("/signup")
+    if(pathname.startsWith("/_next") ) return NextResponse.next();
+    
+
+    // Impedir usuários logados de acessar a página de login (rota "/") e a página de cadastro ("/signup")
     if (pathname === "/" || pathname === "/signup") {
         if (token) {
             const isValid = await validateToken(token);
             if (isValid) {
+                // Redirecionar para o dashboard se o usuário já estiver logado
                 return NextResponse.redirect(new URL("/dashboard", req.url));
             }
         }
         return NextResponse.next();
     }
+    
 
-    // Proteção da rota do dashboard
-    if (pathname.startsWith("/dashboard")) {
-        if (!token) return NextResponse.redirect(new URL("/", req.url));
+   
+    if(pathname.startsWith("/dashboard")){
+        if(!token) return NextResponse.redirect(new URL("/", req.url))
 
-        const isValid = await validateToken(token);
-        if (!isValid) return NextResponse.redirect(new URL("/", req.url));
-    }
+        const isValid = await validateToken(token)
+        if(!isValid) return NextResponse.redirect(new URL("/", req.url))
+    } 
 
     return NextResponse.next();
-}
+} 
 
-async function validateToken(token: string) {
-    if (!token) return false;
+async function validateToken(token: string){
+    if(!token) return false;
 
     try {
-        await api.get("/user/detalhe", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+       await api.get("/user/detalhe",{
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+       }) 
 
-        return true;
+       return true
     } catch (error) {
-        console.log("Erro ao validar o token:", error);
+        console.log(error);
         return false;
     }
 }
